a7af76d136273af3d4e3106b8f64279ab60c4652
keke001009 <94226331+keke001009@users.noreply.github.com>
2022-01-26 19:44:01 +0900

    .

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,11 +1,11 @@
 import { Middleware } from "../deps.ts";
-import { Context } from 'types'
+import { Context } from "types";
 
-export const authenticate: Middleware = async function (ctx : Context, next) {
+export const authenticate: Middleware = async function (ctx: Context, next) {
   console.log("authenticate");
   ctx.user = {
-    id : 'test',
-    permission : 1
-  }
+    id: "test",
+    permission: 1,
+  };
   await next();
 };
--- tests/post.test.ts
+++ tests/post.test.ts
@@ -3,16 +3,14 @@ import {
   assert,
   assertEquals,
 } from "https://deno.land/std@0.122.0/testing/asserts.ts";
-import db from '/db.ts'
-import { 
-  pagingPost
-} from '/domain/services/post.ts'
-import { Database, config } from "/deps.ts";
+import db from "/db.ts";
+import { pagingPost } from "/domain/services/post.ts";
+import { config, Database } from "/deps.ts";
 // await db.sync()
 
 Deno.test("Post 페이징 테스트", async (t) => {
-  await db.sync()
+  await db.sync();
 
-  assertEquals(await pagingPost(), 1)
-  await db.close()
+  assertEquals(await pagingPost(), 1);
+  await db.close();
 });
